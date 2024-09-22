5065ec4c79c2c50680bd82b466a1ecb948d5834c
gravity34031 <gravity2507@gmail.com>
2024-09-22 23:05:53 +0500

    Add project files.

---
+++ Tyuiu.KomarovNA.Sprint3.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Intrinsics.Arm;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KomarovNA.Sprint3.Task0.V29.Lib
+{
+    public class DataService : ISprint3Task0V29
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double sum = 0;
+            double t = value;
+            for (int i = startValue; i<=stopValue; i++)
+            {
+                sum += (Math.Pow(t, 2 * i) + 1 / (i+1)) * Math.Cos(t);
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.KomarovNA.Sprint3.Task0.V29/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.KomarovNA.Sprint3.Task0.V29.Lib;
+namespace Tyuiu.KomarovNA.Sprint3.Task0.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Оператор if - полная и короткая форма записи                            *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу на, которая запрашивает целые значения с клавиатуры и *");
+            Console.WriteLine("* вычисляет находится ли точка с координатами X,Y в заштрихованной области*");
+            Console.WriteLine("*и.                                                                       *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.GetSumSeries(0.5, 1, 10);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
